80bd738beb6be6dbc28514c79ee1b3959a773233
Chintan <cap5192@gmail.com>
2022-06-15 21:32:55 -0500

    test

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 file = open("switch.txt", "r")
 data = file.readlines()
-# [name, int, input ...],[switch01, te1/o, ..\n]...
-formated_data = []
-new_data = []
+each_line = []
 max = 0
 highest_talker = ""
-for i in data:
-    formated_data.append(i.split())
-# [[name, int..],[switch01, te1/0...]
-for i in range(len(formated_data)):
+for i in range(len(data)):
     if i > 0:
-        new_data.append(formated_data[i])
-# [[switch01, te1/0, ..],[switch02...]
+        each_line.append(data[i].split())
+for i in range(len(each_line)):
+    one_line = each_line[i]
+    for x in range(len(one_line)):
+        total = int(one_line[2]) + int(one_line[3])
+        if total > max:
+            max = total
+            highest_talker = one_line[0]
 
-for i in range(len(new_data)):
-    current_data = new_data[i]
-    for x in range(len(current_data)):
-        y = (int(current_data[2]) + int(current_data[3]))
-        if y > max:
-            max = y
-            highest_talker = current_data[0]
-print(f"Highest talker is {highest_talker} with total of {max} Bytes")
+print(f"Highest talker switch is {highest_talker} with total usage of {max}")
 
