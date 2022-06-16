17843125ef100c9f1e64a2db457e1ee136140ecb
Chintan <cap5192@gmail.com>
2022-06-15 22:05:20 -0500

    test

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 file = open("switch.txt", "r")
-data = file.readlines()
-each_line = []
-max = 0
+data = file.readlines()[1:]
+# ['switch01 te1/0 34241 133252\n', 'switch02 te2/0 22343433 522343342\n']
+#split the list so we can recall individual line item
+formatted_list = []
 highest_talker = ""
+max = 0
 for i in range(len(data)):
-    if i > 0:
-        each_line.append(data[i].split())
-for i in range(len(each_line)):
-    one_line = each_line[i]
-    for x in range(len(one_line)):
-        total = int(one_line[2]) + int(one_line[3])
+    formatted_list.append(data[i].split())
+# [['switch01', 'te1/0', '34241', '133252'], ['switch02', 'te2/0', '22343433', '522343342']]
+for i in range(len(formatted_list)):
+    each_list = formatted_list[i]
+    # ['switch01', 'te1/0', '34241', '133252']
+    for x in range(len(each_list)):
+        total = int(each_list[2]) + int(each_list[3])
         if total > max:
             max = total
-            highest_talker = one_line[0]
+            highest_talker = each_list[0]
 
-print(f"Highest talker switch is {highest_talker} with total usage of {max}")
-
+print(highest_talker)
+print(max)
